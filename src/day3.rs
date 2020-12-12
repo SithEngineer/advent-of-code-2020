6202549875411afbedc03e20e342f12459324736
SithEngineer <sithengineer@pm.me>
2020-12-12 12:03:31 +0000

    Solution for day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -4,13 +4,13 @@ mod day3 {
     }
 
     impl Line {
-        pub fn from_string(input: String) -> Line {
+        pub fn from_string(input: &String) -> Line {
             Line {
                 landscape: input.chars().collect()
             }
         }
         pub fn from_slice(input: &str) -> Line {
-            Line::from_string(input.to_string())
+            Line::from_string(&input.to_string())
         }
 
         pub fn is_tree(&self, index: usize) -> bool {
@@ -22,31 +22,43 @@ mod day3 {
         }
     }
 
-    const SLOPE_RIGHT: usize = 3;
-    const SLOPE_DOWN: usize = 1;
-
     pub struct Grid {
+        slope_right: usize,
+        slope_down: usize,
         current_line: usize,
         current_column: usize,
-        pub tree_count: usize,
+        ingested_lines: usize,
+        pub tree_count: u64,
     }
 
     impl Grid {
         pub fn new() -> Grid {
+            Grid::with_slopes(3, 1)
+        }
+
+        pub fn with_slopes(slope_right: usize, slope_down: usize) -> Grid {
             Grid {
+                slope_right,
+                slope_down,
                 current_line: 0,
                 current_column: 0,
+                ingested_lines: 0,
                 tree_count: 0,
             }
         }
 
         pub fn digest(&mut self, line: &Line) {
-            if line.is_tree(self.current_column % line.length()) {
-                self.tree_count += 1;
+            if self.should_process() {
+                if line.is_tree(self.current_column % line.length()) {
+                    self.tree_count += 1;
+                }
+                self.current_line += self.slope_down;
+                self.current_column += self.slope_right;
             }
-            self.current_line += SLOPE_DOWN;
-            self.current_column += SLOPE_RIGHT;
+            self.ingested_lines += 1;
         }
+
+        fn should_process(&self) -> bool { self.ingested_lines == self.current_line }
     }
 }
 
@@ -58,7 +70,7 @@ mod day3_tests {
     #[test]
     fn line_is_created() {
         let input = String::from("#...#");
-        let line = Line::from_string(input);
+        let line = Line::from_string(&input);
         assert!(line.is_tree(0));
         assert!(!line.is_tree(1));
         assert!(!line.is_tree(2));
@@ -107,10 +119,61 @@ mod day3_tests {
 
         let mut grid = Grid::new();
         for line in reader.lines() {
-            grid.digest(&Line::from_string(line.unwrap()))
+            grid.digest(&Line::from_string(&line.unwrap()));
         }
 
         println!("Number of trees encountered {}", grid.tree_count);
-        assert!(grid.tree_count > 0);
+        assert_eq!(294, grid.tree_count);
+    }
+
+    #[test]
+    fn grid_with_custom_slope() {
+        let mut grid = Grid::with_slopes(1, 2);
+        let line = Line::from_slice("#.#..");
+
+        grid.digest(&line);
+        assert_eq!(1, grid.tree_count);
+
+        grid.digest(&line);
+        assert_eq!(1, grid.tree_count);
+
+        grid.digest(&line);
+        assert_eq!(1, grid.tree_count);
+
+        grid.digest(&line);
+        assert_eq!(1, grid.tree_count);
+
+        grid.digest(&line);
+        assert_eq!(2, grid.tree_count);
+    }
+
+    #[test]
+    fn tree_product_of_multiple_slopes() {
+        let file = File::open("resources/day-3-input").unwrap();
+        let reader = BufReader::new(file);
+
+        let mut multi_grid: Vec<Grid> = Vec::new();
+
+        multi_grid.push(Grid::with_slopes(1, 1));
+        multi_grid.push(Grid::with_slopes(3, 1));
+        multi_grid.push(Grid::with_slopes(5, 1));
+        multi_grid.push(Grid::with_slopes(7, 1));
+        multi_grid.push(Grid::with_slopes(1, 2));
+
+        for line in reader.lines() {
+            let parsed_line = Line::from_string(&line.unwrap());
+            for grid in multi_grid.iter_mut() {
+                grid.digest(&parsed_line)
+            }
+        }
+
+        for (index, grid) in multi_grid.iter().enumerate() {
+            println!("Found {} trees in slope {}", grid.tree_count, index);
+        }
+
+        let result = multi_grid.iter().fold(1, |acc, g| acc * g.tree_count);
+
+        println!("Product of number of trees encountered in all slopes {}", result);
+        assert!(result > 1);
     }
 }
